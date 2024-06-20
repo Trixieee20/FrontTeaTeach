6db77b755c712abd4707bdbbf813ccd62458c860
Trixieee20 <trixieemacalalad@gmail.com>
2024-06-20 12:17:53 +0800

    Activity 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,72 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() => runApp(MaterialApp(
     home:Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Center(
-          child: Text('TeaTech App'),
-        ),
+        title: Text('User Profile'),
         backgroundColor: Colors.orange[400],
+        centerTitle: true,
       ),
-      body: Center (
-          child: Image.network('https://mangan.ph/uploads/merchants/5465.jpg')
-          ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-          backgroundColor: Colors.orange[400],
-        child: Icon(
-          Icons.add,
-          color: Colors.white60,
-        ),
+      body: const Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'NAME',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black87,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'Trixie A. Macalalad',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+                fontSize: 20.0
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              'YEAR',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black87,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'Fourth Year',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+                fontSize: 20.0,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              'EMAIL',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.black87,
+                letterSpacing: 2.0,
+              ),
+            ),
+            Text(
+              'trixieemacalalad@gmail.com',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+                fontSize: 20.0,
+              ),
+            ),
+          ],
       ),
-  )));
-}
+      ),
+  ),
+));
+
 
