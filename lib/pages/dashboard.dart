a16bd7b475c3e4612039c93ec5cd9fce3fa412ab
Trixieee20 <trixieeemacalalad@gmail.com>
2024-06-26 11:54:49 +0800

    dashboard and menu

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Home'),
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Container(
+        child:  SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/menu');
+                  },
+                  child: Text('Go to Menu'),
+                ),
+                ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/profile');
+                  },
+                  child: Text('Go to Profile'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
