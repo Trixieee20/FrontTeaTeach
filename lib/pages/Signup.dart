3e31dd83f6335e32c19b274c90be4eaa502cb63c
Trixieee20 <trixieemacalalad@gmail.com>
2024-07-10 12:14:44 +0800

    signup

--- lib/pages/Signup.dart
+++ lib/pages/Signup.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import '../services/user.dart';
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -12,6 +15,23 @@ class _SignupState extends State<Signup> {
   String name = '';
   String email = '';
   String password = '';
+  bool _obscure = true;
+  IconData _obscureIcon = Icons.visibility_off;
+
+  createAccount(User user) async{
+    final response = await http.post(
+      Uri.parse('http://10.0.2.2:8080/api/v1/auth/register/user'),
+      headers:<String, String>{
+        'Content-Type' : 'application/json; charset=UTF-8',
+    },
+        body: jsonEncode(<String,dynamic>{
+          'username' : user.username,
+          'email' : user.email,
+          'password' : user.password,
+        }),
+    );
+    print(response.body);
+  }
 
   @override
 
@@ -79,9 +99,23 @@ class _SignupState extends State<Signup> {
                       ),
                       SizedBox(height: 30.0,),
                       TextFormField(
-                        obscureText: true,
+                        obscureText: _obscure,
                         decoration: InputDecoration(
                           label: Text('Password'),
+                          prefixIcon:  Icon(Icons.lock_rounded),
+                          suffixIcon: IconButton(
+                            icon: Icon(_obscureIcon),
+                            onPressed: (){
+                              setState(() {
+                                _obscure = !_obscure;
+                                if(_obscure){
+                                  _obscureIcon = Icons.visibility_off;
+                                }else{
+                                  _obscureIcon = Icons.visibility;
+                                }
+                              });
+                            },
+                          ),
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(20.0),
                           ),
@@ -107,9 +141,12 @@ class _SignupState extends State<Signup> {
                         onPressed: (){
                           if(formKey.currentState!.validate()){
                             formKey.currentState!.save();
-                            print(name);
-                            print(email);
-                            print(password);
+                             User user = User(
+                              username: name,
+                              email: email,
+                              password: password
+                            );
+                            createAccount(user);
                           }
                         },
                         child: Text('Signup'),
